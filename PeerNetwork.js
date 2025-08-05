f24736c5a978d2059c955f388a0650eb9cd9c9a4
tunntun <mervehtnozcan@gmail.com>
2025-08-05 21:14:34 +0300

    add peerNetwork

--- PeerNetwork.js
+++ PeerNetwork.js
@@ -1,2 +1,43 @@
+const cli = require('nodemon/lib/cli');
 const WebSocket = require('ws');
-const Blockchain = require('./Blockchain');
+
+class PeerNetwork {
+  constructor(blockchain, port){
+    this.blockchain = blockchain;
+    this.sockets = [];
+    this.port = port;
+  }
+  startServer() {
+    const server = new WebSocket.Server({ port: this.port });
+    server.on('connection', (ws) => {
+      this.initSocket(ws)
+    });
+    console.log(`Server running on prot ${this.port}`);
+  }
+
+  connectToPeer (peerUrl) {
+    const client = new WebSocket(peerUrl);
+
+    client.on('open', () => {
+      console.log(`Connected to peer: ${peerUrl}`);
+      this.initSocket(client);
+    });
+    client.on('error', (err) => {
+      console.log('connection_error');
+    });
+  }
+
+  initSocket(ws){
+    this.sockets.push(ws);
+
+    ws.on('message', (message) => {
+      console.log(message.toString());
+    });
+    ws.on('close', ()=> {
+      console.log(`peer disconnected: ${ws}`)
+      this.sockets = this.sockets.filter(s => s !== ws);
+    });
+  }
+}
+
+module.exports = PeerNetwork;
